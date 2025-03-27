29fbc5fffe17d35a553804efdf4b291c67383fff
Nicolly <nicollyengenheira@gmail.com>
2025-03-27 15:03:34 -0300

    Tutorial Django

---
+++ djangotutorial/polls/views.py
@@ -0,0 +1,47 @@
+from django.db.models import F
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from .models import Question, Choice
+from django.urls import reverse
+from django.views import generic
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"]) # retorna o ID da escolha selecionada, como uma string.
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1  #  instrui o banco de dados a aumentar a contagem de votos em 1.
+
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse("polls:results",
+                                            args=(question.id,)))
